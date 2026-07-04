f1cab9985533e725af8884cdd7365529b38ddb7c
bboymwas <mwangigeo1244@gmail.com>
2026-07-04 18:16:39 +0300

    Initial setup

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Welcome to Muso enterprise backend server");
+});
+const port =5000;
+app.listen(port, () => {console.log(`Server is running on port ${port}`)});
